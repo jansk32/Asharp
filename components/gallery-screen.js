5ae05f0dcb9475a3772cb2414540e638f3a7a524
DaphneLucky <dlucky@student.unimelb.edu.au>
2019-09-04 08:57:11 +1000

    Adding grid in Gallery Screen
    Data is currently only letters. Need to add pictures from the back-end.

--- components/gallery-screen.js
+++ components/gallery-screen.js
@@ -1,23 +1,88 @@
 import React, { useState, useEffect } from 'react';
-import { Text, StyleSheet,View } from 'react-native';
+import { Text, StyleSheet,View, FlatList, Dimensions } from 'react-native';
 
+const data = [
+    { key: 'A' },
+    { key: 'B' },
+    { key: 'C' },
+    { key: 'D' },
+    { key: 'E' },
+    { key: 'F' },
+    { key: 'G' },
+    { key: 'H' },
+    { key: 'I' },
+    { key: 'J' },
+    // { key: 'K' },
+    // { key: 'L' },
+  ];
+
+const formatData = (data, numColumns) => {
+    const numberOfFullRows = Math.floor(data.length / numColumns);
+  
+    let numberOfElementsLastRow = data.length % numColumns;
+    while (
+      numberOfElementsLastRow !== numColumns &&
+      numberOfElementsLastRow !== 0
+    ) {
+      data.push({ key: `blank-${numberOfElementsLastRow}`, empty: true });
+      numberOfElementsLastRow++;
+    }
+  
+    return data;
+  };
+
+const numColumns =3;
 
 export default function GalleryScreen() {
+    renderItem = ({ item, index }) => {
+        if (item.empty === true) {
+          return <View style={[styles.item, styles.itemInvisible]} />;
+        }
+        return (
+          <View style={styles.item}>
+            <Text style={styles.itemText}>{item.key}</Text>
+          </View>
+        );
+      };
     return (
-    <View style={styles.container}>
-        <Text style={styles.header}>Memories left behind</Text>
-    </View>
+        <>
+        <View>
+            <Text style={styles.header}>Memories left behind</Text>
+        </View>
+        <FlatList
+            data={formatData(data,numColumns)}
+            renderItem={this.renderItem}
+            numColumns = {numColumns}
+            style = {styles.container}
+        />
+        </>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 10
+        flex: 1,
+        marginVertical:20
     },
     header: {
       fontSize: 20,
-      textAlign:'center'
-    }
+      textAlign:'center',
+      marginVertical:10
+    },
+    item: {
+        backgroundColor: '#4D243D',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        margin: 1,
+        height: Dimensions.get('window').width / numColumns, // approximate a square
+      },
+      itemInvisible: {
+        backgroundColor: 'transparent',
+      },
+      itemText: {
+        color: '#fff',
+      },
   })
   
 
