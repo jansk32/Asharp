55965638a80d499d81aa9323d380081c63e1b0e0
jansk32 <jansenk1@student.unimelb.edu.au>
2019-09-10 19:25:27 +1000

    can do a basic get and post routes

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// user schema
-const schema = require('../schema/userSchema');
+// schemas
+const userSchema = require('../schema/userSchema');
+const artSchema = require('../schema/artefactSchema');
 
 // creates the mongoose model 
-var userModel = mongoose.model('user', schema);
+var userModel = mongoose.model('user', userSchema);
+var artefactModel = mongoose.model('artefact', artSchema);
 
 // to connect to mongodb
 require("../controller/mongooseController");
@@ -22,16 +24,58 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
+// get a user
 app.get('/user', (req, res) => {
-    userModel.find({ name: "Jansen" }, (err, resp) => {
+    // change later
+    userModel.find(req.body, (err, resp) => {
         if (err) throw err;
         res.json(resp);
     });
 })
 
-app.get('/artefact', (req, res) => {
-    res.send("artefact");
+// create a user
+app.post("/user/create", (req,res) => {
+    let user = userModel({
+        name: req.body.name,
+        dob: req.body.dob,
+        userName: req.body.userName,
+        password: req.body.password,
+        children: req.body.children,
+        artefact: req.body.artefact 
+    });
+
+    user.save((err,resp) => {
+        if(err) throw err;
+        res.send(resp);
+    })
 })
+
+// get an artefact
+app.get('/artefact', (req, res) => {
+    artefactModel.find(req.body, (err,resp) => {
+        if (err) throw err;
+        res.json(resp);
+    })
+})
+
+// create an artefact
+app.post("/artefact/create", (req,res) => {
+    let artefact = artefactModel({
+        name: req.body.name,
+        date: req.body.date,
+        owner: req.body.owner,
+        value: req.body.value,
+        description: req.body.description,
+        file: req.body.file
+    });
+
+    artefact.save((err,resp) => {
+        if(err) throw err;
+        res.send(resp);
+    })
+})
+
+// assign artefact to a person
 
 app.listen(port);
 console.log("Listening to port " + port);
--- schema/userSchema.js
+++ schema/userSchema.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: String,
     dob: Date,
+    userName: String,
+    password: String,
     children: [String],
     artefact: [String] 
 });
