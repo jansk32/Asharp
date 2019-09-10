09205cae9244634da9336ad3ccd827e7e6dc639f
jansk32 <jansenk1@student.unimelb.edu.au>
2019-09-10 18:07:14 +1000

    commit profile screen

--- components/profile-screen.js
+++ components/profile-screen.js
@@ -1,15 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, Button } from 'react-native';
+import Axios from 'axios';
 
+this.axios = Axios.create({
+    withCredentials: false,
+    headers:{
+      "Accept": "application/json"
+    },
+    baseURL: 'http://localhost:3000/'
+  });
 
 export default function ProfileScreen() {
+    const [profile, setProfile] = useState("");
+
+    useEffect(() => {
+        console.log('Sending request');
+        let url = fetch("https://localhost:3000/user")
+        .then((resp) => {
+            // setProfile(resp);
+            // console.log(resp);
+            console.log("FOUND");
+        })
+        .catch(error => console.error(error));
+    }, []);
+
     return (
         <>
             <Text style={{ fontSize: 42 }}>This is the profile screen</Text>
+            <Text>{profile} hello</Text>
         </>
     );
 }
 
-ProfileScreen.navigationOptions = {
-    title: 'Profile'
-};
+// ProfileScreen.navigationOptions = {
+//     title: 'Profile'
+// };
