72723074460fbb4508f5433ebc1506970fb5aa5e
DaphneLucky <dlucky@student.unimelb.edu.au>
2019-10-01 09:59:58 +1000

    Update add-family-member-screen.js

--- components/add-family-member-screen.js
+++ components/add-family-member-screen.js
@@ -8,42 +8,48 @@ export default function AddFamilyMemberScreen({ navigation }) {
 
     return(
     <>  
-        <LinearGradient colors={['#436aac','#43b7b8']} style={styles.container}>
-            <Text style={styles.title}>Add family member</Text>
-        </LinearGradient>
+        <View style={styles.allContainer}>
 
-        <View style={styles.inputContainer}>
-            <Text style={styles.header}>Search by email:</Text>
-            <View style={styles.searchContainer}>
-                <Icon name="md-search" size={30} />
-                <TextInput
-                    placeholder='Search username'
-                    style={styles.searchInput}
-                />
+        
+            <View style={styles.container}>
+                <Text style={styles.add}>Find your</Text>
+                <Text style={styles.title}>Family Member</Text>
+                <View style={styles.searchContainer}>
+                    <Icon name="md-search" size={30} color={'#2d2e33'}/>
+                    <TextInput
+                        placeholder='Search by email'
+                        style={styles.searchInput}
+                    />
+                </View>
             </View>
-            
-        
-            <Text style={styles.manualHeader}>Add details manually</Text>
-            <TextInput
-                placeholder='Name'
-                style={styles.textInput}
-            />
-            <TextInput
-                placeholder='Status'
-                style={styles.textInput}
-            />
+        {/* <LinearGradient colors={['#436aac','#43b7b8']} style={styles.container}>
+            <Text style={styles.title}>Add family member</Text>
+        </LinearGradient> */}
 
-        </View>
-        
-        <View style={styles.button}>
-            <Text style={styles.buttonText}>Add</Text>
-        </View>
+            <View style={styles.inputContainer}>   
+                <Text style={styles.manualHeader}>Add details manually</Text>
+                <TextInput
+                    placeholder='Name'
+                    style={styles.textInput}
+                />
+                <TextInput
+                    placeholder='Status'
+                    style={styles.textInput}
+                />
 
+            </View>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>Add</Text>
+            </View>
+        </View>
 
     </>
 )};
 
 const styles = StyleSheet.create({
+    allContainer:{
+        backgroundColor:'#f5f7fb'
+    },
     textInput: {
         borderBottomColor: 'black',
         borderBottomWidth:1,
@@ -57,15 +63,17 @@ const styles = StyleSheet.create({
     searchContainer: {
         flexDirection:'row',
         padding:5,
+        paddingHorizontal:20,
         borderRadius:10,
         borderWidth:1,
         marginLeft:'5%',
-        marginRight:'5%'
-
+        marginRight:'5%',
+        backgroundColor:'#f5f7fb',
+        borderColor:'#f5f7fb'
     },
     searchInput: {
        flex: 1,
-       marginLeft: 5,    
+       marginLeft: 15,    
        padding:5
     },
     header:{
@@ -73,23 +81,35 @@ const styles = StyleSheet.create({
         fontSize:20,
     },
     manualHeader:{
-        marginTop: '10%',
+        // marginTop: '10%',
         padding:10,
         fontSize:20,
     },
     container:{
-        // borderBottomLeftRadius: 40,
-        // borderBottomRightRadius: 40,
+       borderBottomLeftRadius: 30,
+       borderBottomRightRadius: 30,
+       backgroundColor:'white',
+       paddingBottom:30,
     },
     title:{
-        fontSize: 25,
-		textAlign: 'center',
-		color:'white',
-		paddingBottom:'8%',
-		paddingTop:'8%'
+        fontSize: 35,
+		color:'#2d2e33',
+        paddingBottom:'8%',
+        fontWeight:'bold',
+        marginLeft:10,
+    },
+    add:{
+        fontSize:25,
+        color:'#2d2e33',
+        marginLeft:10,
+        marginTop:10,
     },
     inputContainer:{
-        marginTop: '10%'
+        marginTop: '10%',
+        backgroundColor:'white',
+        borderRadius:25,
+        padding:'10%',
+        marginHorizontal: 15,
     },
     buttonText:{
         fontSize: 15,
@@ -106,7 +126,8 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         justifyContent: 'center',
         alignSelf: 'center',
-        marginTop: '20%'
+        marginTop: '20%',
+        marginBottom:'30%'
       },
 });
 
