97eb7b57c3026207f088ddd4ac608fcf305f4159
Raisa <ramalia@student.unimelb.edu.au>
2019-10-08 00:48:48 +1100

    fixed navigation and edit item detail page

--- components/item-detail-screen.js
+++ components/item-detail-screen.js
@@ -2,83 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Text, Image, View, StyleSheet, Dimensions, ScrollView, } from 'react-native';
 import axios from 'axios';
 import Moment from 'moment';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 Moment.locale('en');
-
-const styles = StyleSheet.create({
-    image: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').width,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row',
-    },
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-    },
-    headerCont: {
-        marginHorizontal: '8%',
-        marginTop: '5%',
-        borderBottomColor: '#A4A4A4',
-        borderBottomWidth: .5,
-        justifyContent: 'space-between',
-    },
-    headerDesc: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingBottom: 10,
-    },
-    desc: {
-        justifyContent: 'space-evenly',
-        marginHorizontal: '8%',
-        minHeight: 200,
-    },
-    title: {
-        color: 'black',
-        fontSize: 50,
-        fontWeight: 'bold',
-    },
-    owner: {
-        color: '#579B93',
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    dateStyle: {
-        color: '#EC6268',
-        borderLeftColor: '#fff',
-        alignSelf: 'flex-end',
-        fontWeight: 'bold',
-    },
-    boldHeader: {
-        fontWeight: 'bold',
-        fontSize: 20,
-    },
-    backButt: {
-        borderRadius: 100,
-        backgroundColor: '#fff',
-        position: 'absolute',
-        borderColor: '#ddd',
-        borderBottomWidth: 0,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 1,
-        margin: 30,
-        paddingLeft: 10,
-        paddingTop: 9,
-        width: 40,
-        height: 40,
-    },
-    value:{
-        fontSize: 16,
-    }
-})
-
 
 // See the details of each individual artefacts
 export default function ItemDetailScreen({ navigation }) {
@@ -95,7 +24,7 @@ export default function ItemDetailScreen({ navigation }) {
             // console.log("owner id:"+ res.data.owner);
             const ownerObj = await axios.get('http://localhost:3000/user/artefact', {
                 params: {
-                _id: res.data.owner
+                    _id: res.data.owner
                 }
             });
             await setOwner(ownerObj.data.name);
@@ -104,20 +33,27 @@ export default function ItemDetailScreen({ navigation }) {
         fetchArtefact();
     }, []);
 
-    const backButton = <Icon name="md-home" size={30} />
+    // TODO: Define function to send artefacts to family members
+    const sendArtefact = () => {
+        // @Timothy / @Jansen pls help
+        async function postArtefact() {
+            const res = axios.post(`http://localhost:3000/artefact`, {
+
+            })
+        }
+    }
+
 
     return (
         <>
-            <View style={styles.container}>
-                <Image
-                    style={styles.image}
-                    source={{ uri: artefact.file }}
-                />
-
-                <ScrollView>
-
+            <ScrollView>
+                <View style={styles.container}>
+                    <Image
+                        style={styles.image}
+                        source={{ uri: artefact.file }}
+                    />
                     <View style={styles.headerCont}>
-                    <Text style={styles.title}>{artefact.name}</Text>
+                        <Text style={styles.title}>{artefact.name}</Text>
 
                         <View style={styles.headerDesc}>
                             <Text style={styles.owner}>Owned by {owner}</Text>
@@ -126,24 +62,103 @@ export default function ItemDetailScreen({ navigation }) {
                         </View>
                     </View>
                     <View style={styles.desc}>
-                        <View>
-                            <Text style={styles.boldHeader}>Description:</Text>
-                            <Text>{artefact.description}</Text>
-                        </View>
-                        <View>
-                            <Text style={styles.boldHeader}>Value:</Text>
-                            <Text>{artefact.value}</Text>
-                        </View>
+                        <Text style={styles.boldHeader}>Description:</Text>
+                        <Text style={styles.descriptionStyle}>{artefact.description}</Text>
                     </View>
-                </ScrollView>
-                <Icon
-                    name="md-arrow-round-back"
-                    size={22}
-                    style={styles.backButt}
-                    onPress={() => navigation.goBack()}
-                />
-            </View>
+                    <View style={styles.desc}>
+                        <Text style={styles.boldHeader}>Value:</Text>
+                        <Text style={styles.descriptionStyle}>{artefact.value}</Text>
+                    </View>
+                    <View style={styles.buttonBox}>
+                        <TouchableOpacity
+                            onPress={() => navigate('Home')}
+                            style={styles.sendButton}>
+                            <Text style={{ color: 'white', textAlign: 'center', fontSize: 18 }}>
+                                Send Artefacts
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View >
+            </ScrollView>
         </>
     );
 }
+const styles = StyleSheet.create({
+    image: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    headerCont: {
+        paddingHorizontal: '8%',
+        paddingVertical: '5%',
+        marginBottom: '5%',
+        // backgroundColor: '#E6E6E6',
+        borderColor: 'black',
+        borderBottomEndRadius: 30,
+        borderBottomStartRadius: 30,
+        borderWidth: 1,
+        justifyContent: 'space-between',
+    },
+    headerDesc: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingBottom: 10,
+    },
+    desc: {
+        marginHorizontal: '8%',
+        marginVertical: 20,
+        justifyContent: 'space-between',
+    },
+    descriptionStyle: {
+        marginTop: 5,
+        padding: 10,
+        paddingBottom: 30,
+        borderRadius: 5,
+        borderColor: 'black',
+        borderWidth: 0.5
+    },
+    title: {
+        color: 'black',
+        fontSize: 50,
+        fontWeight: 'bold',
+    },
+    owner: {
+        color: 'black',
+        fontSize: 18,
+    },
+    dateStyle: {
+        color: '#579B93',
+        borderLeftColor: '#fff',
+        alignSelf: 'flex-end',
+        fontWeight: 'bold',
+    },
+    boldHeader: {
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    value: {
+        fontSize: 16,
+    },
+    buttonBox: {
+        justifyContent: 'center',
+        marginHorizontal: 20,
+        marginVertical: 40,
+    },
+    sendButton: {
+        backgroundColor: '#EC6268',
+        width: Dimensions.get('window').width / 1.75,
+        height: Dimensions.get('window').width / 8,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignSelf: 'center',
+    },
+
+})
 
