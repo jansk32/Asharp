8750867f33592d1d11ff46a9fa59bcc18bd7850a
Raisa <ramalia@student.unimelb.edu.au>
2019-10-15 21:37:04 +1100

    Co-authored-by: DaphneLucky <dlucky@student.unimelb.edu.au>

--- components/welcome-screen.js
+++ components/welcome-screen.js
@@ -1,24 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Dimensions, ToastAndroid } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Dimensions, ToastAndroid, Image } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import AsyncStorage from '@react-native-community/async-storage';
 import { axiosLocal } from './log-in-screen';
 
 const styles = StyleSheet.create({
     title: {
-        marginTop: '20%',
-        textAlign: 'center',
-        fontSize: 50,
-        marginBottom: '75%',
-        flexDirection: 'row',
+        marginTop: 60,
+        marginBottom: 50,
+        width: 300,
+        height: 60,
+        alignSelf:'center'
+    },
+    image: {
+        width: Dimensions.get('window').width /1.5, 
+        height: Dimensions.get('window').width /1.5,
+        alignSelf: 'center',
+        justifyContent:'center',
+        marginBottom: 50,
+        marginTop: 20,
     },
     yellowButton: {
         textAlign: 'center',
-        borderWidth: 1,
+        // borderWidth: 1,
         borderRadius: 50,
         width: Dimensions.get('window').width / 1.75,
         height: Dimensions.get('window').width / 8,
-        borderColor: '#fbc074',
-        backgroundColor: '#fbc074',
+        // borderColor: '#fbc074',
+        // backgroundColor: '#fbc074',
         justifyContent: 'center',
         alignSelf: 'center',
     },
@@ -28,7 +37,7 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         width: Dimensions.get('window').width / 1.75,
         height: Dimensions.get('window').width / 8,
-        borderColor: '#fbc074',
+        borderColor: '#579B93',
         backgroundColor: 'white',
         justifyContent: 'center',
         alignSelf: 'center',
@@ -38,15 +47,23 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     buttonBox: {
-        backgroundColor: 'white',
+        // backgroundColor: 'white',
         flex: 1,
-        justifyContent: 'space-evenly',
         alignItems: 'center',
-        paddingVertical: '5%',
+        justifyContent: 'space-evenly',
+        paddingVertical: 30,
     },
     buttonText: {
         fontSize: 16,
         textAlign: 'center',
+        backgroundColor:'transparent',
+        color:'white',
+    },
+    buttonText2: {
+        fontSize: 16,
+        textAlign: 'center',
+        backgroundColor:'transparent',
+        color:'black',
     },
 });
 
@@ -73,21 +90,36 @@ export default function WelcomeScreen({ navigation }) {
     return (
         <>
             <View style={styles.container}>
-                <Text style={styles.title}>mementos</Text>
+                <Image style={styles.title} source={require('../TEXT.png')}/>
+                <Image style={styles.image}
+                source={require('../reverse-icon.png')}/>
                 {
                     !isLoading &&
-                    (
-                        <View style={styles.buttonBox}>
-                            <TouchableOpacity onPress={() => navigate('SignUp1')}>
-                                <View style={styles.yellowButton}>
-                                    <Text style={styles.buttonText}>I am a new user </Text>
-                                </View>
-                            </TouchableOpacity>
+                    (   
+                        <View style = {styles.buttonBox}>
+                        <TouchableOpacity onPress={() => navigate('SignUp1')}>
+                            <LinearGradient colors={['#06beb6', '#48b1bf']} style={styles.yellowButton}>
+
+                            {/* <View style={styles.yellowButton}> */}
+                                <Text style={styles.buttonText}>I am a new user </Text>
+                            {/* </View> */}
+                            </LinearGradient>
+                        </TouchableOpacity>
+                    
+
+
+
+                            
                             <TouchableOpacity onPress={() => navigate('Login')}>
-                                <View style={styles.whiteButton}>
-                                    <Text style={styles.buttonText}>I am an existing user</Text>
+                            <View style={styles.whiteButton}>
+
+                                {/* <View style={styles.whiteButton}> */}
+                                    <Text style={styles.buttonText2}>I am an existing user</Text>
+                                {/* </View> */}
                                 </View>
+
                             </TouchableOpacity>
+
                         </View>
                     )
                 }
