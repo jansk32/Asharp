472aa842fd0f4fef923551bf97d20e622343f8d2
Raisa <ramalia@student.unimelb.edu.au>
2019-10-16 12:23:52 +1100

    tab bar view

--- components/add-family-member-screen.js
+++ components/add-family-member-screen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Text, StyleSheet, View, Image, TouchableOpacity, TextInput, Dimensions, ScrollView, Alert } from 'react-native';
 import UserSearchBox from './user-search-box';
 import axios from 'axios';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 
 import DatePicker from 'react-native-datepicker';
 import Moment from 'moment';
@@ -65,6 +66,109 @@ export default function AddFamilyMemberScreen({ navigation }) {
             </TouchableOpacity>
         );
     }
+    function SearchMemberRoute(){
+        return(
+            <ScrollView>
+            <View>
+                <UserSearchBox renderItem={renderSearchResult} />
+            </View>
+
+        </ScrollView>
+        )
+        
+    }
+    function AddMemberRoute() {
+        return(
+            <View>
+            <View style={styles.inputContainer}>
+                <Text style={styles.manualHeader}>Add details manually</Text>
+                <TextInput
+                    placeholder="Name"
+                    style={styles.textInput}
+                    value={name}
+                    onChangeText={setName}
+                />
+                <DatePicker
+                    style={styles.dateInputs}
+                    date={dob}
+                    mode="date"
+                    placeholder={Moment().format(DATE_FORMAT)}
+                    format={DATE_FORMAT}
+                    maxDate={Moment().format(DATE_FORMAT)}
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    androidMode="spinner"
+                    customStyles={{
+                        dateIcon: {
+                            position: 'absolute',
+                            left: 0,
+                            top: 4,
+                            marginLeft: 0
+                        },
+                        dateInput: {
+                            // borderColor: 'white',
+                        }
+                    }}
+                    showIcon={false}
+                    onDateChange={newDate => setDob(newDate)}
+                    value={dob}
+                />
+                {linkedNode.spouse ?
+                    <TextInput
+                        placeholder="Gender"
+                        style={styles.textInput}
+                        value={gender}
+                        onChangeText={setGender}
+                    />
+                    :
+                    null
+                }
+                <TextInput
+                    placeholder="Picture"
+                    style={styles.textInput}
+                />
+            </View>
+            <View style={styles.button}>
+                <Text
+                    style={styles.buttonText}
+                    onPress={() => {
+                        const newUserInfo = {
+                            name,
+                            dob,
+                        };
+
+                        if (linkedNode.spouse) {
+                            // Linked node has a spouse, so add a child
+                            // Father and mother are already entered
+                            newUserInfo.father = linkedNode.gender === 'm' ? linkedNode._id : linkedNode.spouse;
+                            newUserInfo.mother = linkedNode.gender === 'f' ? linkedNode._id : linkedNode.spouse;
+                            newUserInfo.gender = gender;
+                        } else {
+                            // Linked node doesn't have a spouse, so add a spouse
+                            // Spouse is already available
+                            // Gender of the new user is implicitly the opposite of the linked node's
+                            newUserInfo.spouse = linkedNode._id;
+                            newUserInfo.gender = linkedNode.gender === 'm' ? 'f' : 'm';
+                        }
+                        axios.post('http://asharp-mementos.herokuapp.com/user/create', newUserInfo);
+                        navigation.goBack();
+                    }}>
+                    Add
+                        </Text>
+            </View>
+        </View>
+        )
+        
+            
+    }
+
+    const [tab, setTab] = useState({
+        index: 0,
+        routes: [
+            { key: 'first', title: 'Search Family Member' },
+            { key: 'second', title: 'Add Manually' },
+        ],
+    });
 
     return (
         <>
@@ -72,87 +176,24 @@ export default function AddFamilyMemberScreen({ navigation }) {
                 <Text style={styles.add}>Find your</Text>
                 <Text style={styles.title}>{isAddingSpouse ? 'Spouse' : 'Child'}</Text>
             </View>
-            <ScrollView>
-                    <View>
-                        <UserSearchBox renderItem={renderSearchResult} />
-                    </View>
-                <View style={styles.inputContainer}>
-                    <Text style={styles.manualHeader}>Add details manually</Text>
-                    <TextInput
-                        placeholder="Name"
-                        style={styles.textInput}
-                        value={name}
-                        onChangeText={setName}
-                    />
-                    <DatePicker
-                        style={styles.dateInputs}
-                        date={dob}
-                        mode="date"
-                        placeholder={Moment().format(DATE_FORMAT)}
-                        format={DATE_FORMAT}
-                        maxDate={Moment().format(DATE_FORMAT)}
-                        confirmBtnText="Confirm"
-                        cancelBtnText="Cancel"
-                        androidMode="spinner"
-                        customStyles={{
-                            dateIcon: {
-                                position: 'absolute',
-                                left: 0,
-                                top: 4,
-                                marginLeft: 0
-                            },
-                            dateInput: {
-                                // borderColor: 'white',
-                            }
-                        }}
-                        showIcon={false}
-                        onDateChange={newDate => setDob(newDate)}
-                        value={dob}
-                    />
-                    {linkedNode.spouse ?
-                        <TextInput
-                            placeholder="Gender"
-                            style={styles.textInput}
-                            value={gender}
-                            onChangeText={setGender}
-                        />
-                        :
-                        null
-                    }
-                    <TextInput
-                        placeholder="Picture"
-                        style={styles.textInput}
-                    />
-                </View>
-                <View style={styles.button}>
-                    <Text
-                        style={styles.buttonText}
-                        onPress={() => {
-                            const newUserInfo = {
-                                name,
-                                dob,
-                            };
-
-                            if (linkedNode.spouse) {
-                                // Linked node has a spouse, so add a child
-                                // Father and mother are already entered
-                                newUserInfo.father = linkedNode.gender === 'm' ? linkedNode._id : linkedNode.spouse;
-                                newUserInfo.mother = linkedNode.gender === 'f' ? linkedNode._id : linkedNode.spouse;
-                                newUserInfo.gender = gender;
-                            } else {
-                                // Linked node doesn't have a spouse, so add a spouse
-                                // Spouse is already available
-                                // Gender of the new user is implicitly the opposite of the linked node's
-                                newUserInfo.spouse = linkedNode._id;
-                                newUserInfo.gender = linkedNode.gender === 'm' ? 'f' : 'm';
-                            }
-                            axios.post('http://asharp-mementos.herokuapp.com/user/create', newUserInfo);
-                            navigation.goBack();
-                        }}>
-                        Add
-                        </Text>
-                </View>
-            </ScrollView>
+            <TabView
+				navigationState={tab}
+				renderScene={SceneMap({
+					first: SearchMemberRoute,
+					second: AddMemberRoute,
+				})}
+				renderTabBar={props =>
+					<TabBar
+						{...props}
+						indicatorStyle={{ backgroundColor: '#EC6268' }}
+						style={{ backgroundColor: '#f5f7fb' }}
+						bounces={true}
+						labelStyle={{ color: '#2d2e33' }}
+					/>
+				}
+				onIndexChange={index => setTab({ ...tab, index })}
+				initialLayout={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height }}
+			/>
         </>
     )
 };
@@ -163,15 +204,15 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
     },
     textInput: {
-		borderColor: 'black',
-		borderWidth: 0.5,
-		borderRadius: 3,
-		alignContent: 'center',
-		padding: 5,
-		paddingLeft: 10,
+        borderColor: 'black',
+        borderWidth: 0.5,
+        borderRadius: 3,
+        alignContent: 'center',
+        padding: 5,
+        paddingLeft: 10,
         width: Dimensions.get('window').width / 2,
-        marginBottom:20,
-	},
+        marginBottom: 20,
+    },
     searchContainer: {
         flexDirection: 'row',
         padding: 5,
@@ -218,8 +259,8 @@ const styles = StyleSheet.create({
         borderRadius: 25,
         marginHorizontal: 15,
         marginTop: 20,
-        alignSelf:'center',
-        justifyContent:'space-evenly',
+        alignSelf: 'center',
+        justifyContent: 'space-evenly',
     },
     buttonText: {
         fontSize: 15,
@@ -238,8 +279,8 @@ const styles = StyleSheet.create({
         marginVertical: 20,
     },
     dateInputs: {
-		alignContent: 'center',
+        alignContent: 'center',
         width: Dimensions.get('window').width / 2,
         marginBottom: 20,
-	},
+    },
 });
